2c4ad4dceeeb1f50c265042ef970854f2e30452a
Felipe Andres Rodriguez Yaguache <46670671+felipeary91@users.noreply.github.com>
2023-04-09 17:21:06 +0300

    Add files via upload
    Modified the header to allow delivering the Frequency Response of the filter.

--- Averaging_filter_header.h
+++ Averaging_filter_header.h
@@ -5,24 +5,30 @@
 #include <omp.h>
 
 
-void MATLAB_main(double*, double*, size_t, double);
+void MATLAB_main(mxComplexDouble*, double*, double*, size_t, double, double);
 
+#include "mex.h"
 
 void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
-	double multiplier[1];
+	
+	double multiplier[2];	//multiplier is the array of scalar inputs
 	mxDouble* y, * z;	//y is the input and z is the output
+	mxComplexDouble* w;	//w is the complex vector output 
 	size_t mrows, ncols, numb_elements;
 
-	//Verifying that we have two inputs and one output
-	if (nrhs != 2) {
+	//Verifying that we have three inputs and two outputs
+	if (nrhs != 3) {
 		mexErrMsgIdAndTxt("MATLAB:xtimesy:invalidNumInputs", "Invalid number of inputs.");
 	}
-	if (nlhs != 1) {
-		mexErrMsgIdAndTxt("MATLAB:xtimesy:invalidNumOutputs", "Invalid number of outputs. One is required.");
+	if (nlhs != 2) {
+		mexErrMsgIdAndTxt("MATLAB:xtimesy:invalidNumOutputs", "Invalid number of outputs. Two are required.");
 	}
 
 	//Getting the value from sizeBuffer
+	//and number of points for the frequency
+	//response
 	multiplier[0] = mxGetScalar(prhs[1]);
+	multiplier[1] = mxGetScalar(prhs[2]);
 
 	//Getting the pointer to the input array
 	y = mxGetDoubles(prhs[0]);
@@ -42,5 +48,10 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
 	//Getting the pointer to a copy of the output array
 	z = mxGetDoubles(plhs[0]);
 
-	MATLAB_main(z, y, numb_elements, multiplier[0]);
+	//Setting the pointer to the complex output array
+	plhs[1] = mxCreateDoubleMatrix(1, multiplier[1] + 1, mxCOMPLEX);
+	//Getting the pointer to the complex output array
+	w = mxGetComplexDoubles(plhs[1]);
+
+	MATLAB_main(w, z, y, numb_elements, multiplier[0], multiplier[1]);
 }
